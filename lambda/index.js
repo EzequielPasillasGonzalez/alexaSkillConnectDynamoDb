1a850e376d602ff62715a7b7f9263f5ae383a2e3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-04-04 17:59:25 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -77,34 +77,34 @@ const FallbackIntentHandler = {
  * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
  * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
  * */
-//const SessionEndedRequestHandler = {
-    //canHandle(handlerInput) {
-        //return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
-    //},
-    //handle(handlerInput) {
-        //console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
-        // Any cleanup logic goes here.
-      //  return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
-    //}
-    //
-//};
-
 const SessionEndedRequestHandler = {
-    async handle(handlerInput){
-
-    const attributesManager = handlerInput.attributesManager;
-    const attributes = await attributesManager.getPersistentAttributes() || {};
-    console.log('attributes is: ', attributes);
-
-    const counter = attributes.hasOwnProperty('counter')? attributes.counter : 0;
-
-    let speechOutput = `Hi there, Hello World! Your counter is ${counter}`;
-
-    return handlerInput.responseBuilder
-        .speak(speechOutput)
-        .getResponse();
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
     }
-}
+    
+};
+
+//const SessionEndedRequestHandler = {
+    //async handle(handlerInput){
+
+    //const attributesManager = handlerInput.attributesManager;
+//    const attributes = await attributesManager.getPersistentAttributes() || {};
+    //console.log('attributes is: ', attributes);
+
+    //const counter = attributes.hasOwnProperty('counter')? attributes.counter : 0;
+
+//    let speechOutput = `Hi there, Hello World! Your counter is ${counter}`;
+
+  //  return handlerInput.responseBuilder
+//        .speak(speechOutput)
+      //  .getResponse();
+    //}
+//}
 
 
 const GetServiceIntentHandler = {
